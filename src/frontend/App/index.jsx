dc809d7b0ea6a10a3dc1c1e0ca5af0bc28832c3a
Anashen Navasartian Brown <anashnavasart@gmail.com>
2025-10-01 19:01:32 -0700

    L1 Sun And Moon Complete

--- src/frontend/App/index.jsx
+++ src/frontend/App/index.jsx
@@ -1,44 +1,39 @@
+import { useState } from "react";
 import styled from "styled-components";
-import { HelmetProvider } from "react-helmet-async";
 
-// components
-import Meta from "@Core/components/Meta";
-
-//styles
-const AppStyled = styled.div`
-    position: fixed;
-    left: 0px;
-    right: 0px;
-    top: 0px;
-    bottom: 0px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    color: white;
-    background: linear-gradient(to bottom, #007676, #004141);
-    font-family: Arial, Helvetica, sans-serif;
-    margin: auto;
+const SunAndMoonStyled = styled.div`
     text-align: center;
-    h1 {
-        font-size: 50px;
+    h2 {
+        font-size: 30px;
+        color: teal;
+    }
+    img {
+        display: block;
+        margin: 30px auto;
+        padding: 20px;
+        border: solid 20px teal;
+        background-color: #013636;
     }
 `;
 
-// component
-const App = () => {
-    const pageTitle = `UCLAX Web 1: ${
-        import.meta.env.VITE_APP_CONFIG_STUDENT_NAME
-    }`;
+const SunAndMoon = () => {
+    const [srcState, setsrcState] = useState("/assets/sunAndMoon/moon.png");
+    const changeToSun = () => {
+        setsrcState("/assets/sunAndMoon/sun.png");
+    };
+    const changeToMoon = () => {
+        setsrcState("/assets/sunAndMoon/moon.png");
+    };
     return (
-        <HelmetProvider>
-            <AppStyled>
-                <Meta pageTitle={pageTitle} />
-                <h1>{pageTitle}</h1>
-            </AppStyled>
-        </HelmetProvider>
+        <SunAndMoonStyled>
+            <h2>Sun And Moon</h2>
+            <img
+                src={srcState}
+                alt="Sun And Moon"
+                onMouseEnter={changeToSun}
+                onMouseLeave={changeToMoon}
+            />
+        </SunAndMoonStyled>
     );
 };
-
-export default App;
+export default SunAndMoon;
